a08601f79ab5ccac78ec6418c6189c6e833273c5
Yaser Haddad <mohamedyaserhaddad@gmail.com>
2021-02-03 22:10:57 +0100

    prueba

--- Banca/src/App.java
+++ Banca/src/App.java
@@ -1,5 +1,31 @@
+import java.lang.ref.Cleaner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println("Empezamos proyecto...");
+        System.out.println("");
+        CuentaBancaria Bankia = new CuentaBancaria("1", "Bankia", 1890.75);
+        CuentaBancaria Rural = new CuentaBancaria("2", "Rural", 1774.88);
+        CuentaBancaria CoinBase = new CuentaBancaria("3", "CoinBaser", 84.57);
+        System.out.println("");
+
+        System.out.println(Bankia.toString());
+        System.out.println(Rural.toString());
+        System.out.println(CoinBase.toString());
+        System.out.println("");
+
+        double cantBankia = Bankia.getCantidad();
+        double cantRural = Rural.getCantidad();
+        double cantCoinBase = CoinBase.getCantidad();
+        System.out.println("");
+
+        double total = sumaTotal(cantBankia, cantCoinBase, cantRural);
+        System.out.println("Dinero total: " + total);
+        System.out.println("");
+
+        System.out.println("Dinero con la beca: " + (total + 1700));
+    }
+    public static Double sumaTotal(double a, double b, double c) {
+        return a + b + c;
     }
 }
